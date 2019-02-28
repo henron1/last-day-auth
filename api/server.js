3b6f443800fd09f601bc6637074c8ecd9e796854
Henry Neal <henry.neal0@gmail.com>
2019-02-28 16:13:06 -0500

    finished mvp

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const configureMiddleware = require('./middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+// defined the secret
+const secrets = require('../config/secrets.js');
+
+const server = express();
+
+configureMiddleware(server);
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+module.exports = server;
